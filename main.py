967c93cefa3b8172fef1098eff15a3e68f1c9491
FaridCG343 <71570619+FaridCG343@users.noreply.github.com>
2022-06-04 23:41:35 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,109 +1,52 @@
+import math
+
 comV = []
 dicM = {}
-m = [1,5,17]
-ValorB = 20
+m = [8, 6, 7]
+ValorB = 95
 
 
-def agregar(temporal):
-    settemp={0}
-    for a in temporal:
-        settemp.add(a)
-    for sus in settemp:
-        if sus in dicM.keys():
-            temporal.remove(sus)
-            for sus2 in dicM[sus]:
-                temporal2=temporal.copy()
-                for sus3 in sus2:
-                    temporal2.append(sus3)
-                temporal2.sort(reverse=True)
-                if temporal2 in comV:
-                    pass
-                else:
-                    suma=0
-                    for sum in temporal2:
-                        suma+=sum
-                    if suma==ValorB:
-                        comV.append(temporal2.copy())
-                agregar(temporal2.copy())
-
-
-def crearcomV(monedas):
-    for i in range(len(monedas)):
-        valor = ValorB
-        cmb = []
-        for y in range(i, len(monedas)):
-            xd = True
-            valor2 = monedas[y]
-            while xd:
-                if valor >= valor2:
-                    cmb.append(valor2)
-                    valor = valor - valor2
-                else:
-                    xd = False
+def combinacion(monedas, num1=ValorB, comM=[]):
+    if len(monedas) > 0:
+        if len(monedas) == 1:
+            for x in range(math.floor(num1 / monedas[0]), 0, -1):
+                if (num1 - (x * monedas[0])) == 0:
+                    for y in range(x, 0, -1):
+                        comM.append(monedas[0])
+                    comV.append(comM.copy())
+                    comM.clear()
         else:
-            if valor == 0:
-                if cmb in comV:
-                    pass
+            for x in range(math.floor(num1 / monedas[0]), 0, -1):
+                if (num1 - (x * monedas[0])) == 0:
+                    for y in range(x, 0, -1):
+                        comM.append(monedas[0])
+                    comV.append(comM.copy())
+                    comM.clear()
                 else:
-                    comV.append(cmb)
-                agregar(cmb.copy())
-                del cmb
-    else:
-        i = True
-        while i:
-            for a in range(len(comV)):
-                i = False
-                if len(comV[a]) == 1:
-                    comV.pop(a)
-                    i = True
-                    break
-
-
-def crearDicM(monedas):
-    for x in (range((len(monedas) - 1))):
-        comM = []
-        for i in range(1, len(monedas)):
-            valor = monedas[x]
-            cmb = []
-            for y in range(i, len(monedas)):
-                xd = True
-                valor2 = monedas[y]
-                while xd:
-                    if valor >= valor2:
-                        cmb.append(valor2)
-                        valor = valor - valor2
-
-                    else:
-                        xd = False
-            else:
-                if valor == 0:
-                    comM.append(cmb)
-                    del cmb
-        else:
-            i = True
-            while i:
-                i = False
-                if len(comM)==0:
-                    pass
-                else:
-                    for a in range(len(comM)):
-                        if len(comM[a]) == 1:
-                            comM.pop(a)
-                            i = True
+                    mtemp = monedas.copy()
+                    mtemp.remove(monedas[0])
+                    for y in range(x, 0, -1):
+                        comM.append(monedas[0])
+                    print("ComM",comM)
+                    num1-=(monedas[0]*x)
+                    mtemp2=monedas.copy()
+                    for a in range(0,len(mtemp2)):
+                        if monedas[a] <= num1:
+                            combinacion(monedas=mtemp2.copy(), num1=num1, comM=comM.copy())
                             break
-            if comM==[]:
-                pass
-            else:
-                dicM[moneda[x]] = comM
-            del comM
+                        else:
+                            mtemp2.remove(monedas[a])
+                comM.clear()
 
 
-moneda = [x for x in m if x <= ValorB]
+moneda = [x for x in m if ValorB >= x > 0]
 moneda.sort(reverse=True)
-crearDicM(moneda)
-print(dicM)
-crearcomV(moneda)
-i=1
+while len(moneda)>0:
+    combinacion(monedas=moneda)
+    moneda.pop(0)
+    print(moneda)
 for x in comV:
-    print(i, x)
-    i+=1
+    suma=0
+    for y in x:
+        suma+=y
+    print(suma,x)
