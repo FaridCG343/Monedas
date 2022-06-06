065c9bb4aa52c7b3828edc56a0eae234617cfd6d
FaridCG343 <71570619+FaridCG343@users.noreply.github.com>
2022-06-06 14:06:39 -0600

    Update main.py
    arreglando un bug donde se hacia infinito

--- main.py
+++ main.py
@@ -1,37 +1,97 @@
 comV = []
 dicM = {}
-m = [1,5,17]
+mcm = {}
+m = [1,2,11]
 ValorB = 20
 
 
+def agregarMcM():
+        for x in comV:
+            aux = []
+            for cn in x:
+                aux.append(cn)
+            for y in mcm:
+                for a in mcm[y]:
+                    cvqc = y // a
+                    if aux.count(a) >= cvqc:
+                        ltemp = aux.copy()
+                        for b in range(cvqc):
+                            ltemp.remove(a)
+                        for c in mcm[y]:
+                            ltemp2 = ltemp.copy()
+                            if c != a:
+                                for d in range(y // c):
+                                    ltemp2.append(c)
+                                ltemp2.sort(reverse=True)
+                                if ltemp2 in comV:
+                                    pass
+                                else:
+                                    comV.append(ltemp2.copy())
+
+
+def maximo_comun_divisor(a, b):
+    while b != 0:
+        temporal = b
+        b = a % b
+        a = temporal
+    return a
+
+
+def minimo_comun_multiplo(a, b):
+    return (a * b) / maximo_comun_divisor(a, b)
+
+
+def crearMcM(m1):
+    for x in range(len(m1)-1):
+        for y in range(x+1, len(m1)):
+            mcmtemp = int(minimo_comun_multiplo(m1[x], m1[y]))
+            if mcmtemp <= ValorB:
+                if mcmtemp in m1:
+                    pass
+                else:
+                    z = mcm.keys()
+                    if mcmtemp in z:
+                        au = {0}
+                        au.update(mcm[mcmtemp])
+                        au.add(m1[x])
+                        au.add(m1[y])
+                        au.remove(0)
+                        mcm[mcmtemp] = au
+                    else:
+                        mcm[mcmtemp] = {m1[x], m1[y]}
+
+
 def agregar(temporal):
-    settemp={0}
+    settemp = {0}
     for a in temporal:
         settemp.add(a)
+    settemp.remove(0)
     for sus in settemp:
         if sus in dicM.keys():
             temporal.remove(sus)
             for sus2 in dicM[sus]:
-                temporal2=temporal.copy()
+                temporal2 = temporal.copy()
                 for sus3 in sus2:
                     temporal2.append(sus3)
                 temporal2.sort(reverse=True)
                 if temporal2 in comV:
                     pass
                 else:
-                    suma=0
-                    for sum in temporal2:
-                        suma+=sum
-                    if suma==ValorB:
+                    suma = 0
+                    for sux in temporal2:
+                        suma += sux
+                    if suma == ValorB:
                         comV.append(temporal2.copy())
-                agregar(temporal2.copy())
+                        agregar(temporal2.copy())
 
 
 def crearcomV(monedas):
     for i in range(len(monedas)):
+
         valor = ValorB
         cmb = []
         for y in range(i, len(monedas)):
+            print("xd")
             xd = True
             valor2 = monedas[y]
             while xd:
@@ -53,7 +113,10 @@ def crearcomV(monedas):
         while i:
             for a in range(len(comV)):
                 i = False
-                if len(comV[a]) == 1:
+                su = 0
+                for x in comV[a]:
+                    su += x
+                if su != ValorB:
                     comV.pop(a)
                     i = True
                     break
@@ -83,7 +146,7 @@ def crearDicM(monedas):
             i = True
             while i:
                 i = False
-                if len(comM)==0:
+                if len(comM) == 0:
                     pass
                 else:
                     for a in range(len(comM)):
@@ -91,19 +154,25 @@ def crearDicM(monedas):
                             comM.pop(a)
                             i = True
                             break
-            if comM==[]:
+            if not comM:
                 pass
             else:
                 dicM[moneda[x]] = comM
             del comM
 
 
-moneda = [x for x in m if x <= ValorB]
+moneda = [x for x in m if 0 < x <= ValorB]
 moneda.sort(reverse=True)
+crearMcM(moneda)
 crearDicM(moneda)
 print(dicM)
+print(mcm)
 crearcomV(moneda)
+agregarMcM()
 i=1
 for x in comV:
-    print(i, x)
+    suma = 0
+    for y in x:
+        suma += y
+    print(i,suma, x)
     i+=1
